03539d05b14262b0df54572d003b5601e5fa3967
datbh06 <bdat1606@gmail.com>
2023-08-03 15:48:45 +0700

    completed home__focus
    working on home__juncture

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 let currentDate = new Date();
 let options = {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric'};
 let dateString = currentDate.toLocaleDateString('vi-VN', options);
 document.getElementById('currentDateHeader').innerHTML = dateString;
+
